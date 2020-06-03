0c6dfc2702c13aad0b95c7257a5b4b4029942789
krakopo <krakopo@protonmail.com>
2020-06-03 01:01:24 -0400

    Add build and install commands to helper script

--- apk.sh
+++ apk.sh
@@ -1,9 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-set -xe
+set -e
+
+action=$1
 
 # Path to Android SDK
 export ANDROID_HOME=$HOME/android/sdk
 
-gradle wrapper
-./gradlew build
+if [ ! -f ./gradlew ]
+then
+	gradle wrapper
+fi
+
+case "$action" in
+	"build")
+		./gradlew build
+		;;
+	"install")
+  		./gradlew installDebug
+		;;
+	*)
+		echo "Usage: $0 {build|install}"
+		exit 1
+esac
